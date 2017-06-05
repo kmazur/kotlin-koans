b48362b966849630f46fdaffbb398d29b0b6d371
Krzysztof Mazur <mazurkrzysztof.k@gmail.com>
2017-06-05 20:55:08 +0200

    III conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,37 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    operator fun plus(interval: TimeInterval): MyDate {
+        return this.addTimeIntervals(interval, 1)
+    }
+
+    operator fun plus(interval: RepeatedTimeInterval): MyDate {
+        return this.addTimeIntervals(interval.interval, interval.times)
+    }
+
+    override fun compareTo(other: MyDate): Int {
+        val yearComp = year.compareTo(other.year)
+        if(yearComp != 0) {
+            return yearComp
+        }
+        val monthComp = month.compareTo(other.month)
+        if(monthComp != 0) {
+            return monthComp
+        }
+        val dayComp = dayOfMonth.compareTo(other.dayOfMonth)
+        if(dayComp != 0) {
+            return dayComp
+        }
+        return 0
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +39,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(n: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(this, n)
+}
+
+data class RepeatedTimeInterval(val interval: TimeInterval, val times: Int) {
+
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+
+        var current: Optional<MyDate> = Optional.empty<MyDate>()
+        return object : Iterator<MyDate> {
+
+            override fun next(): MyDate {
+                current = Optional.of<MyDate>(current.map { it.nextDay() }.orElse(start))
+                return current.orElse(start)
+            }
+
+            override fun hasNext(): Boolean {
+                return current.orElse(start) < endInclusive
+            }
+
+        };
+    }
+}
